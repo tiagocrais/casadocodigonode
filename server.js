793c284ef2afdd839ac2fd419b8128ecf415999f
Tiago Crais <tiagocrais@gmail.com>
2020-10-25 23:44:46 -0300

    Implementando função do módulo express no Server.js.

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log(`Servidor rodando na porta 3000`);
+});
+
+app.get('/', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+        </html>    
+    `);
+});
+
+/*
 const http = require('http');
 
 const servidor = http.createServer(function(req, resp) {
@@ -13,3 +34,4 @@ const servidor = http.createServer(function(req, resp) {
     `);
 });
 servidor.listen(3000);
+*/
